a26817ec7c8480886c4af475bdee00e2ed9954b0
Ryan Almansa <ryan.almansa@gmail.com>
2025-12-02 16:06:53 +0100

    affichage console debut de renvoie de reponse

--- front/script.js
+++ front/script.js
@@ -1,1 +1,10 @@
-alert("pipi caca");
+fetch("/api/test")
+.then(res => res.json())
+.then(data => {
+    console.log("Réponse API :");
+    console.log(data.message);
+    console.log(data.toto);
+    console.log(data.nombre);
+    console.log(data.liste);
+})
+.catch(err => console.error("Erreur API :", err));
